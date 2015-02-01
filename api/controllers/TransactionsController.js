22020d6ba4cc0fd62598fa9ecfa63e3cba07c57d
BalinorNZ <balinornz@gmail.com>
2015-02-01 22:21:42 +1300

    0.1
    date pickers, sorting with column headers, filter income and expenses

--- api/controllers/TransactionsController.js
+++ api/controllers/TransactionsController.js
@@ -5,6 +5,58 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+function cleanData (data) {
+	var myData = [];
+
+	var transactor;
+	if(data[0] == '4988-****-****-2726'){
+		//nicTotal += parseFloat(data[2]);
+		transactor = 'Nic';
+	} else if (data[0] == '4988-****-****-2734') {
+		//katieTotal += parseFloat(data[2]);
+		transactor = 'Katie';
+	} else if (data[0] == '4988-****-****-2718') {
+		transactor = 'Direct';
+	}
+	myData[0] = transactor;
+
+	var ind = myIndexOf(myData, data[3])
+	var amount = 0;
+	if(data[1] == 'D'){
+		amount = -Math.abs(parseFloat(data[2]));
+	} else if(data[1] == 'C') {
+		amount = parseFloat(data[2]);
+	}
+	if(ind > -1) {
+		myData[ind].amount += amount;
+		myData[ind].count++;
+	} else {
+		myDatapoint = { vendor: data[3], 
+						amount: amount,
+						count: 1,
+						};
+		myData.push(myDatapoint);
+	}
+
+	var dataObj = { transactor: myData[0],
+					direction: data[1],
+					amount: amount,
+					vendor: data[3],
+					date: data[4],
+					date1: data[5], };
+
+	return dataObj;
+}
+
+function myIndexOf(arr, o) {    
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].vendor == o) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 module.exports = {
 	get: function(req, res) {
 
@@ -12,14 +64,10 @@ module.exports = {
 		var csv = require("fast-csv");
 		var rawData = [];
 		csv.fromPath("credit.CSV").on("data", function(data){
-			var dataObj = { transactor: data[0],
-							direction: data[1],
-							amount: data[2],
-							vendor: data[3],
-							date: data[4],
-							date1: data[5], };
 
-			rawData.push(dataObj);
+			var clean = cleanData(data);
+
+			rawData.push(clean);
 		}).on("end", function(){
 			return res.json({ transactions: rawData });
 		});
@@ -74,15 +122,6 @@ module.exports = {
 	    					nic: nicTotal.toFixed(2) });
 		});
 
-		function myIndexOf(arr, o) {    
-		    for (var i = 0; i < arr.length; i++) {
-		        if (arr[i].vendor == o) {
-		            return i;
-		        }
-		    }
-		    return -1;
-		}
-	
 	},
 	index: function (req, res) {
 		return res.view({item: "test2"});
--- assets/js/viewmodel.js
+++ assets/js/viewmodel.js
@@ -1,15 +1,149 @@
 function myViewmodel() {
 	var self = this;
 
-	self.title = 'Transactions';
+	self.title = 'Transactions Tracker';
 	self.transactions = ko.observable();
+	self.displayTransactions = ko.observable();
+
+	self.transactionsTotal = ko.computed(function() {
+		var total = 0;
+		_.each(self.displayTransactions(), function(transaction) {
+			total += parseFloat(transaction.amount);
+		});
+		return total.toFixed(2);
+	}, self);
+
+	self.vendorsCount = ko.computed(function() {
+		var grouped = _.groupBy(self.displayTransactions(), function(transaction) {
+			return transaction.vendor;
+		});
+		return Object.keys(grouped).length;
+	});
+
+	self.transactionsCount = ko.computed(function() {
+		var transactionsCount = 0;
+		if(typeof self.displayTransactions() != "undefined") {
+			transactionsCount = Object.keys(self.displayTransactions()).length;
+		}
+		return transactionsCount;
+	});
+
+	self.tables = ['Table1', 'Table2', 'Table3'];
+	self.chosenTableId = ko.observable();
+	self.showIncome = ko.observable(true);
+	self.showExpenses = ko.observable(true);
+	var today = new Date('today');
+	console.log('today: '+today);
+	self.startDate = ko.observable(getToday(-1));
+	self.endDate = ko.observable(getToday());
+
+	function getToday(years){
+		years = typeof years !== 'undefined' ? years : 0;
+		var today = new Date();
+		var dd = today.getDate();
+		var mm = today.getMonth()+1; //January is 0!
+		var yyyy = today.getFullYear()+years;
+
+		if(dd<10) { dd='0'+dd } 
+
+		if(mm<10) { mm='0'+mm } 
+
+		today = dd+'/'+mm+'/'+yyyy;
+		return today;
+	}
 
 	// get transactions list
 	io.socket.get('/transactions/get', function(data, res){
-		console.log(data);
 		self.transactions(data.transactions);
-		//self.transactions();
+		self.displayTransactions(self.transactions());
 	});
+
+	function getDate(dateStr) {
+		var year = parseInt(dateStr.substr(6, 9));
+		var month = parseInt(dateStr.substr(3, 5))-1;
+		var day = parseInt(dateStr.substr(0, 2));
+		var date = new Date(year, month, day);
+		return date;
+	}
+
+	function dateFilter(transactions) {
+		console.log('date filtering '+self.startDate()+' - '+self.endDate());
+
+		filtered = _.reject(transactions, function(transaction){
+			var date = getDate(transaction.date);
+			var startDate = getDate(self.startDate());
+			var endDate = getDate(self.endDate());
+
+			if(date > endDate) { return true;}
+			if(date < startDate) { return true; }
+
+		    return false;
+		});
+
+		return filtered;
+	}
+
+	self.resetDates = function() {
+		self.startDate(getToday(-1));
+		self.endDate(getToday());
+		self.filterTable();
+		return true;
+	}
+
+	self.filterTable = function(data, event) {
+		console.log('filtering table');
+
+		var filtered = self.transactions();
+
+		// filter out income if income not selected
+		if(!self.showIncome()){
+			filtered = _.reject(filtered, function(transaction){
+			    return parseFloat(transaction.amount) > 0 ? true : false; 
+			});
+		}
+
+		// filter out expenses if expenses not selected
+		if(!self.showExpenses()){
+			filtered = _.reject(filtered, function(transaction){
+			    return parseFloat(transaction.amount) > 0 ? false : true; 
+			});
+		}
+
+		self.displayTransactions(dateFilter(filtered));
+
+		// return true so the checkbox event isn't cancelled
+		return true;
+	}
+
+	self.sortTable = function(data, event) {
+		// sort by selected property
+		var prop = event.target.innerText.toLowerCase();
+		console.log('sorting table by '+prop);
+		if(prop == 'date') {
+			var sorted = _.sortBy(self.displayTransactions(), function(transaction){
+				var date = getDate(transaction.date);
+				return date;
+			});
+		} else {
+			var sorted = _.sortBy(self.displayTransactions(), prop);
+		}
+
+		self.displayTransactions(dateFilter(sorted));
+	};
+
+	self.groupTable = function(data, event) {
+		// stub to use underscore _.groupBy later
+	}
+
+	self.selectTable = function(table) {
+		self.chosenTableId(table);
+		console.log(table);
+		$(".table").hide();
+		$(".table."+table).show();
+	};
+
+	// initialize
+	self.selectTable('Table1');
 };
 
 ko.applyBindings(new myViewmodel());
