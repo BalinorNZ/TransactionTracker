9bf061a61719fb4e09f41b5ad195a548629642df
BalinorNZ <balinornz@gmail.com>
2015-07-26 21:38:24 +1200

    change transactions location in bootstrap

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,3 +1,5 @@
+/* global Import */
+/* global sails */
 /**
  * Bootstrap
  * (sails.config.bootstrap)
@@ -9,46 +11,46 @@
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
-module.exports.bootstrap = function(cb) {
+module.exports.bootstrap = function (cb) {
 
   var retry = 0;
 
-  function importCSV(filename){
-  	sails.config.globals.DB_LOCK = true;
-	Import.import(filename, function(err){
-  		if(err) sails.log.error(err);
-	});
+  function importCSV(filename) {
+    sails.config.globals.DB_LOCK = true;
+    Import.import(filename, function (err) {
+      if (err) sails.log.error(err);
+    });
   }
 
-  var tryImport = function(filename){
-  	if(!sails.config.globals.DB_LOCK){
-      	importCSV(filename);
-  	} else {
-  		sails.log.info('DB locked, trying again in a second.');
-  		retry++;
-  		if(retry <= 1) {
-  			setTimeout(function(){ tryImport(filename); }, 1000);
-  		} else {
-  			sails.log.info('Reached maximum retrys.');
-  			retry = 0;
-  		}
-  	}
+  var tryImport = function (filename) {
+    if (!sails.config.globals.DB_LOCK) {
+      importCSV(filename);
+    } else {
+      sails.log.info('DB locked, trying again in a second.');
+      retry++;
+      if (retry <= 1) {
+        setTimeout(function () { tryImport(filename); }, 1000);
+      } else {
+        sails.log.info('Reached maximum retrys.');
+        retry = 0;
+      }
+    }
   }
 
-  function scanCsv(){
+  function scanCsv() {
     var fs = require('fs');
-    fs.watch('./combo.CSV', function(event, filename){
-      if(filename === null) filename = 'combo.CSV';
+    fs.watch('./transactions/combo.CSV', function (event, filename) {
+      if (filename === null) filename = 'combo.CSV';
 
       sails.log.info(event, 'detected for file', filename);
 
-      if(event != 'change') return;
+      if (event != 'change') return;
 
-  	  tryImport(filename);
+      tryImport(filename);
 
     });
   }
-  
+
   scanCsv();
 
   // It's very important to trigger this callback method when you are finished
